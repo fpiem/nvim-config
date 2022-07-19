e68256a76bd82047e48b189a89aa75c43ff58476
Francesco Piemontese <piemontesefrancesco@outlook.com>
2022-07-19 18:45:58 +0200

    swap : and ;

--- lua/user/keymaps.lua
+++ lua/user/keymaps.lua
@@ -20,6 +20,9 @@ vim.g.maplocalleader = " "
 
 -- Normal --
 -- Better window navigation
+keymap("n", ";", ":", { noremap = true, silent = false })
+keymap("n", ":", ";", { noremap = true, silent = false })
+
 keymap("n", "<C-h>", "<C-w>h", opts)
 keymap("n", "<C-j>", "<C-w>j", opts)
 keymap("n", "<C-k>", "<C-w>k", opts)
@@ -66,4 +69,3 @@ keymap("x", "<A-k>", ":move '<-2<CR>gv-gv", opts)
 -- keymap("t", "<C-j>", "<C-\\><C-N><C-w>j", term_opts)
 -- keymap("t", "<C-k>", "<C-\\><C-N><C-w>k", term_opts)
 -- keymap("t", "<C-l>", "<C-\\><C-N><C-w>l", term_opts)
-
