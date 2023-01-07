3a83fa6122983f533930a9142bf12192c9fae7fa
Francesco Piemontese <piemontesefrancesco@outlook.com>
2023-01-07 14:03:36 +0100

    init v2

--- lua/user/illuminate.lua
+++
@@ -1,5 +0,0 @@
--- Highlight variable under the cursor
-vim.api.nvim_set_hl(0, "IlluminatedWordText", { link = "Visual" })
-vim.api.nvim_set_hl(0, "IlluminatedWordRead", { link = "Visual" })
-vim.api.nvim_set_hl(0, "IlluminatedWordWrite", { link = "Visual" })
-
