27ec164bfc203211a6f9dc03b494d7a9b8aea241
Francesco Piemontese <piemontesefrancesco@outlook.com>
2023-01-11 10:22:14 +0100

    add buffer navigation mappings

--- lua/fpiem/remaps.lua
+++ lua/fpiem/remaps.lua
@@ -1,6 +1,13 @@
 -- Set space as the leader
 vim.g.mapleader = " "
 
-vim.keymap.set("n", ":", ";")
-vim.keymap.set("n", ";", ":")
+local opts = { noremap = true, silent = true }
 
+vim.keymap.set("n", ":", ";", opts)
+vim.keymap.set("n", ";", ":", opts)
+vim.keymap.set("v", ":", ";", opts)
+vim.keymap.set("v", ";", ":", opts)
+
+-- Buffer navigation
+vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
+vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
