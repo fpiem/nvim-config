2893326ea6a7fdb13299c04e7a0ec098427b55da
Francesco Piemontese <piemontesefrancesco@outlook.com>
2023-01-11 16:47:26 +0100

    add window navigation mappings

--- lua/fpiem/remaps.lua
+++ lua/fpiem/remaps.lua
@@ -11,3 +11,10 @@ vim.keymap.set("v", ";", ":", opts)
 -- Buffer navigation
 vim.keymap.set("n", "<S-l>", ":bnext<CR>", opts)
 vim.keymap.set("n", "<S-h>", ":bprevious<CR>", opts)
+
+-- Better window navigation
+vim.keymap.set("n", "<C-h>", "<C-w>h", opts)
+vim.keymap.set("n", "<C-j>", "<C-w>j", opts)
+vim.keymap.set("n", "<C-k>", "<C-w>k", opts)
+vim.keymap.set("n", "<C-l>", "<C-w>l", opts)
+
