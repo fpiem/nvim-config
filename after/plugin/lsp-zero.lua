08b947566f4e38c0107b5d2b50ebca5cf64ee3ea
Francesco Piemontese <piemontesefrancesco@outlook.com>
2023-01-16 00:55:30 +0100

    better lspzero settings (to be improved)

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -1,8 +1,21 @@
 local status_ok, lsp_zero = pcall(require, "lsp-zero")
 if not status_ok then
-	return
+  return
 end
 
 lsp_zero.preset('recommended')
+
+lsp_zero.set_preferences({
+  set_lsp_keymaps = { omit = { 'K' } }  -- K is used for vertical navigation
+})
+
+lsp_zero.setup_nvim_cmp({
+  sources = {
+    { name = "path" },
+    { name = "nvim_lsp", keyword_length = 1 },
+    { name = "buffer", keyword_length = 3 },
+  }
+})
+
 lsp_zero.setup()
 
