d05fee8b238f3d19d9fd92638fbd53eb07d86895
Francesco Piemontese <piemontesefrancesco@outlook.com>
2023-01-25 18:05:10 +0100

    preference lsp mappings

--- after/plugin/lsp-zero.lua
+++ after/plugin/lsp-zero.lua
@@ -3,6 +3,14 @@ if not status_ok then
   return
 end
 
+local cmp = require("cmp")
+local cmp_select = { behavior = cmp.SelectBehavior.Select }
+local cmp_mappings = lsp_zero.defaults.cmp_mappings({
+  ["<C-k>"] = cmp.mapping.select_prev_item(cmp_select),
+  ["<C-j>"] = cmp.mapping.select_next_item(cmp_select),
+  ["<C-Space>"] = cmp.mapping.complete()
+})
+
 lsp_zero.preset('recommended')
 
 lsp_zero.set_preferences({
@@ -14,7 +22,8 @@ lsp_zero.setup_nvim_cmp({
     { name = "path" },
     { name = "nvim_lsp", keyword_length = 1 },
     { name = "buffer", keyword_length = 3 },
-  }
+  },
+  mapping = cmp_mappings
 })
 
 lsp_zero.setup()
