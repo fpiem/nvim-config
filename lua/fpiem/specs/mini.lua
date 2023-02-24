f0c496e23b0f017d44f31b9189010fa3f22fe55c
Francesco Piemontese <piemontesefrancesco@outlook.com>
2023-02-24 12:03:03 +0100

    add mini.bracketed

--- lua/fpiem/specs/mini.lua
+++ lua/fpiem/specs/mini.lua
@@ -5,7 +5,7 @@ return {
       require("mini.move").setup()
       require("mini.comment").setup()
       require("mini.bufremove").setup()
-
+      require("mini.bracketed").setup()
       vim.keymap.set("n", "<leader>bd", ":lua require('mini.bufremove').delete()<CR>", { desc = "Delete buffer" })
     end,
     lazy = false,
